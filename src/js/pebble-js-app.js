59fd9700a6d186e6dc24fc7ae43e42ce03228090
Willdorf <william.g.armstrong@gmail.com>
2015-11-05 12:14:01 -0500

    update where configuration page is currently hosted

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-	var url = 'http://f2665c86.ngrok.io';
+	var url = 'http://armstrongwill.com/pebbleconfigurationpages/minimalskulls/index.html';
 
 	console.log('Showing configuration page: ' + url);
 
